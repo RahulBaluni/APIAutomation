631f2a4de25ea0e9f3f9795b4c4b1cc756c7cac1
Nitin Goyal <n.goyal@dnata.com>
2021-01-05 22:34:33 +0400

    fixed test case run issue

--- src/test/java/payload/CreateUserPayload.java
+++ src/test/java/payload/CreateUserPayload.java
@@ -4,15 +4,10 @@ import lombok.Builder;
 import lombok.Setter;
 
 @Setter
-@Builder(toBuilder = true)
+@Builder
 public class CreateUserPayload {
-<<<<<<< HEAD
-    private String name;
-    private String job;
-=======
 
     private String name;
     private String job;
 
->>>>>>> ab5f8dcd8816e9409b383935c3f76d49186455ca
 }
